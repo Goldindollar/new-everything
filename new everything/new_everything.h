42744dd8f8ac784216209f9ec1572d8dfcc3bfb0
goldindollar <mister@goldindollar.com>
2023-08-31 03:03:30 -0700

    Initial Commit

---
+++ new everything/new_everything.h
@@ -0,0 +1,18 @@
+//
+//  new_everything.h
+//  new everything
+//
+//  Created by Goldin Dollar on 8/31/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for new_everything.
+FOUNDATION_EXPORT double new_everythingVersionNumber;
+
+//! Project version string for new_everything.
+FOUNDATION_EXPORT const unsigned char new_everythingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <new_everything/PublicHeader.h>
+
+
